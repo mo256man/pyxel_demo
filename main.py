05d15677093466990e8695bc91763c80da73a4f8
mo256man <48610559+mo256man@users.noreply.github.com>
2025-11-23 10:35:11 +0900

    Create simple Pyxel game in main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import pyxel
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 120)
+        pyxel.load("assets.pyxres")  # Load your assets here
+        self.x = 50
+        self.y = 50
+        self.speed = 2
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        if pyxel.btn(pyxel.KEY_LEFT):
+            self.x -= self.speed
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            self.x += self.speed
+        if pyxel.btn(pyxel.KEY_UP):
+            self.y -= self.speed
+        if pyxel.btn(pyxel.KEY_DOWN):
+            self.y += self.speed
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.circ(self.x, self.y, 5, 7)  # Draw a moving circle
+
+App()
